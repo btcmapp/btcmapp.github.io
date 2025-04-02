a2275cf6d0429265b82be120dc4b1a11e2c11532
cdbCBLR4tvhvZNEjBaoe <74746167+cdbCBLR4tvhvZNEjBaoe@users.noreply.github.com>
2025-04-02 18:47:14 +0800

    updated

---
+++ js/useIncrease-Ca_rbqAq.js
@@ -0,0 +1,1 @@
+import{C as a}from"./CardFactory-B5Mnixjl.js";import{u as n}from"./nft-7oStJogB.js";function o(){const{data:e,refetch:r}=n({...a,functionName:"increase"});return{increase:e??0n,refetch:r}}export{o as u};
---
+++ js/useWriteContract-DR11iGh-.js
@@ -0,0 +1,1 @@
+import{a as c,b as e}from"../index-DJzJbLZs.js";import{w as s}from"./writeContract-BFVkJB_T.js";function m(t){return{mutationFn(n){return s(t,n)},mutationKey:["writeContract"]}}function w(t={}){const{mutation:n}=t,o=c(t),r=m(o),{mutate:i,mutateAsync:u,...a}=e({...n,...r});return{...a,writeContract:i,writeContractAsync:u}}export{w as u};
