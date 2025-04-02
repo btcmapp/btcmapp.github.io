cf585484b660a074e081c4877802ada99eed6101
cdbCBLR4tvhvZNEjBaoe <74746167+cdbCBLR4tvhvZNEjBaoe@users.noreply.github.com>
2025-04-02 19:13:16 +0800

    updated

---
+++ js/useWriteContract-BwqSZP1T.js
@@ -0,0 +1,1 @@
+import{a as c,b as e}from"../index-BXFkDR67.js";import{w as s}from"./writeContract-DisDav7G.js";function m(t){return{mutationFn(n){return s(t,n)},mutationKey:["writeContract"]}}function w(t={}){const{mutation:n}=t,o=c(t),r=m(o),{mutate:i,mutateAsync:u,...a}=e({...n,...r});return{...a,writeContract:i,writeContractAsync:u}}export{w as u};
