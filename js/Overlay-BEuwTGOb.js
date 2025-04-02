badc0dc0c516e0074cf886ac912a47c7bedf93a9
cdbCBLR4tvhvZNEjBaoe <74746167+cdbCBLR4tvhvZNEjBaoe@users.noreply.github.com>
2025-04-02 10:30:47 +0800

    updated

---
+++ js/Overlay-BEuwTGOb.js
@@ -0,0 +1,1 @@
+import{j as e}from"../index-3MgPFwnk.js";import{X as i}from"./transition-B8hIq1zs.js";const n=({visible:t,children:s,onClose:r})=>e.jsx(i,{show:t,children:e.jsx("div",{onClick:r,className:"fixed inset-0 top-0 left-0 z-[60] flex h-screen w-screen items-center justify-center bg-[#050505]/60 transition-opacity data-[closed]:opacity-0",children:s})});export{n as default};
---
+++ js/useWriteContract-CS3_1NR6.js
@@ -0,0 +1,1 @@
+import{a as c,b as e}from"../index-3MgPFwnk.js";import{w as s}from"./writeContract-B2JjUB2g.js";function m(t){return{mutationFn(n){return s(t,n)},mutationKey:["writeContract"]}}function w(t={}){const{mutation:n}=t,o=c(t),r=m(o),{mutate:i,mutateAsync:u,...a}=e({...n,...r});return{...a,writeContract:i,writeContractAsync:u}}export{w as u};
